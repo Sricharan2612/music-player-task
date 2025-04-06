a07058bf1da39577efcfe902eb559b44c5f0a083
Sricharan2612 <sricharany2000@gmail.com>
2025-04-06 18:40:42 +0530

    made a small change to the song component

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import song1 from '../assets/song1.png';
 import { ContextState } from '../context/Context';
+import { playAudio } from '../util';
 
 
 const Song = ({ song }) => {
-    const { setCurrentSong, songs, setSongs } = ContextState();
+    const { setCurrentSong, songs, setSongs, isPlaying, audioRef } = ContextState();
     const handleSongSelection = () => {
         setCurrentSong(song);
 
@@ -22,6 +23,7 @@ const Song = ({ song }) => {
             }
         });
         setSongs(setNewActiveSongs);
+        playAudio(isPlaying, audioRef);
     };
     return (
         <div className={`playlist-song ${song.active ? 'active-song' : ''}`} onClick={handleSongSelection}>
